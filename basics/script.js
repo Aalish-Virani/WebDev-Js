e59b187fa19b50293afe70be2d439212bf86a544
Aalish Virani <126311156+Aalish-Virani@users.noreply.github.com>
2023-09-24 09:11:35 +0000

    js strings

--- basics/script.js
+++ basics/script.js
@@ -1,2 +1,24 @@
-let str = "Aalish"
+let str = ""
 let strToNum = Number(str);
+console.table([str, typeof strToNum, strToNum])
+
+str = "Aalish"
+strToNum = Number(str);
+
+console.table([str, typeof strToNum, strToNum])
+
+str = ""
+let strToBool = Boolean(str);
+console.table([str, typeof strToBool, strToBool])
+
+str = "Aalish"
+strToBool = Boolean(str);
+
+console.table([str, typeof strToBool, strToBool])
+
+
+let str2 = "Virani"
+
+let str3 = {...str, ...str2}
+
+console.log(str3);
