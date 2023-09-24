40241db67fe813d01430ed90c3c3aa55a30cc8e6
Aalish Virani <126311156+Aalish-Virani@users.noreply.github.com>
2023-09-24 12:31:37 +0000

    objects

--- basics/script.js
+++ basics/script.js
@@ -23,3 +23,29 @@ let str3 = new String(str1 + " " + str2)
 
 console.log(str3);
 
+
+
+
+
+// Objects
+
+let deviceCode = Symbol("device1")
+
+let laptop = {
+    brand : "lenovo",
+    processor : "i7",
+    generation : 5,
+    [deviceCode] : "device1",
+    memory : {
+        ram : "8 gb",
+        ssd : "256 gb",
+        hdd : false
+    },
+}
+
+laptop.color = "black",
+
+console.table([ [laptop.brand], [laptop["processor"]],[laptop[deviceCode]] , [laptop.memory.ssd],[laptop.memory.ssd], [laptop.color]])
+console.log(Object.keys(laptop))
+console.log(Object.values(laptop))
+console.log(Object.entries(laptop))
